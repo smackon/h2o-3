b93aeceba24f793003e239e7511af0e8fb0b2ade
wendycwong <wendycwong@yahoo.com>
2018-05-15 12:10:38 -0700

    Shrink dataset size for Hadoop multinode.

--- h2o-py/tests/testdir_hdfs/pyunit_INTERNAL_HDFS_PUBDEV_5233_xgboost_multinodes_random_seeds.py
+++ h2o-py/tests/testdir_hdfs/pyunit_INTERNAL_HDFS_PUBDEV_5233_xgboost_multinodes_random_seeds.py
@@ -1,5 +1,5 @@
 import random
-import sys
+import sys, time
 
 from h2o.estimators.xgboost import *
 from tests import pyunit_utils
@@ -17,8 +17,8 @@ def randomDataset_random_seeds_test():
     else:
         sys.exit("Hadoop cluster is not available!")
 
-    nrow = 10000000
-    ncol = 1000
+    nrow = 1000000  # Megan suggests to use 5million rows and 1000 columns
+    ncol = 100  # in order to make Hadoop test, need to make this 1 million rows by 100 columns
     trainFile = pyunit_utils.random_dataset_numeric_only(nrow, ncol, integerR=1000000, misFrac=0)
     trainFile.set_name(0,"response") # randomly choose a response column
     n = trainFile.nrow
@@ -38,18 +38,31 @@ def randomDataset_random_seeds_test():
     h2oModel1 = H2OXGBoostEstimator(**h2oParams)
     # gather, print and save performance numbers for h2o model
     h2oModel1.train(x=myX, y=y, training_frame=trainFile)
+    h2oTrainTime = h2oModel1._model_json["output"]["run_time"]
+    time1 = time.time()
     h2oPredict1 = h2oModel1.predict(trainFile)
+    h2oPredictTime = time.time()-time1
+    print("Model train time is {0} and scoring time is {1}.".format(h2oTrainTime, h2oPredictTime))
+
     h2oModel2 = H2OXGBoostEstimator(**h2oParams)
     # gather, print and save performance numbers for h2o model
     h2oModel2.train(x=myX, y=y, training_frame=trainFile)
+    h2oTrainTime = h2oModel2._model_json["output"]["run_time"]
+    time1 = time.time()
     h2oPredict2 = h2oModel2.predict(trainFile)
+    h2oPredictTime = time.time()-time1
+    print("Model train time is {0} and scoring time is {1}.".format(h2oTrainTime, h2oPredictTime))
+
     h2oParams2 = {"ntrees":100, "max_depth":10, "learn_rate":0.7, "col_sample_rate_per_tree" : 0.9,
                  "min_rows" : 5, "score_tree_interval": 100, "seed":seed2}
     h2oModel3 = H2OXGBoostEstimator(**h2oParams2)
     # gather, print and save performance numbers for h2o model
     h2oModel3.train(x=myX, y=y, training_frame=trainFile)
+    h2oTrainTime = h2oModel3._model_json["output"]["run_time"]
+    time1 = time.time()
     h2oPredict3 = h2oModel3.predict(trainFile)
-
+    h2oPredictTime = time.time()-time1
+    print("Model train time is {0} and scoring time is {1}.".format(h2oTrainTime, h2oPredictTime))
     # Result comparison in terms of prediction output.  In theory, h2oModel1 and h2oModel2 should be the same
     # h2oModel3 will be different from the other two models
 
